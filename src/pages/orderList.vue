44255f2059267da0b5dd9129b7e4c18670c69541
Haohao-555 <3283627490@qq.com>
2021-09-15 10:34:25 +0800

    订单列表无数据优化及数据加载动画

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -8,7 +8,7 @@
     <div class="wrapper">
       <div class="container">
         <div class="order-box">
-          <!-- <loading v-if="loading"></loading> -->
+          <loading v-if="loading"></loading>
           <div class="order" v-for="(order,index) in list" :key="index">
             <div class="order-title">
               <div class="item-info fl">
@@ -46,6 +46,7 @@
               </div>
             </div>
           </div>
+          <no-data v-if="!loading && list.length ==0"></no-data>
         </div>
       </div>
     </div>
@@ -53,14 +54,19 @@
 </template>
 <script>
 import orderHeader from "./../components/OrderHeader";
+import Loading from './../components/Loading'
+import NoData from './../components/NoData'
 export default {
     name:'order-list',
     components: {
       orderHeader,
+      Loading,
+      NoData
     },
     data() {
         return {
-            list: []
+            list: [],
+            loading:true,
         }
     },
     mounted() {
@@ -69,7 +75,10 @@ export default {
     methods: {
         getOrderList() {
             this.axios.get("/orders").then(res => {
+                this.loading = false;
                 this.list = res.list
+            }).catch(() => {
+                this.loading = false;
             })
         },
         goPay(orderNo) {
