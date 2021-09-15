d3e267ae12f6b738a5d980043edbfa52d46e69b0
Haohao-555 <3283627490@qq.com>
2021-09-15 10:21:24 +0800

    订单列表的数据渲染

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -13,7 +13,6 @@ import navFooter from "./../components/NavFooter";
 export default {
   name: "order",
   components: {
-   
     navFooter,
     serviceBar,
   },
@@ -27,19 +26,7 @@ export default {
   
   },
   methods: {
-    // init() {
-    //   let path = this.$route.path;
-    //   if (path == "/order/confirm") {
-    //     this.title = "订单确定";
-    //     this.tip = "请认真填写地址";
-    //   } else if (path == "/order/list") {
-    //     this.title = "订单列表";
-    //     this.tip = "请谨防钓鱼链接或诈骗电话，链接更多";
-    //   } else if (path == "/order/pay") {
-    //     this.title = "订单支付";
-    //     this.tip = "请谨防钓鱼链接或诈骗电话，链接更多";
-    //   }
-    // },
+  
   },
 };
 </script>
